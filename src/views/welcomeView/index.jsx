1e4139811112907d861209809d8419688b217b07
Jaime Millán <jmillangalvez@gmail.com>
2023-03-16 20:44:22 +0100

    ADD naviagation and send them to the buttons

--- src/views/welcomeView/index.jsx
+++ src/views/welcomeView/index.jsx
@@ -3,17 +3,17 @@ import { WelcomeButton, WelcomeHeader, FlexView } from '../../components';
 import { styles } from "./styles";
 import { labels } from "../../labels/labels";
 
-const WelcomeView = () => {
+const WelcomeView = ({ navigation }) => {
     return (
         <View style={styles.view}>
             <FlexView flex='4.5'></FlexView>
             <WelcomeHeader text={labels.appName}>
             </WelcomeHeader>
             <FlexView flex='4.5'></FlexView>
-            <WelcomeButton text={labels.login}>
+            <WelcomeButton text={labels.login} navigation={navigation}>
             </WelcomeButton>
             <FlexView flex='0.5'></FlexView>
-            <WelcomeButton text={labels.register}>
+            <WelcomeButton text={labels.register} navigation={navigation}>
             </WelcomeButton>
             <FlexView flex='5'></FlexView>
         </View>
