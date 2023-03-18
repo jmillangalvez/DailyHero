ab6551d0e2f6e987e2f6fde9791527fabadaa53f
Jaime Millán <jmillangalvez@gmail.com>
2023-03-18 16:43:49 +0100

    ADD redux reducers

---
+++ src/redux/reducers/welcomeInputReducer.js
@@ -0,0 +1,43 @@
+import { welcomeInputActions } from "../actions/welcomeInputActions";
+
+export function welcomeInputReducer(state = [], action) {
+    console.log(action);
+    console.log(state);
+    switch (action.type){
+        case welcomeInputActions.MODIFY_USERNAME_INPUT:
+            return {
+                ...state,
+                welcomeInputs: {
+                    ...state.welcomeInputs,
+                    usernameInput: action.text
+                }
+            };
+        case welcomeInputActions.MODIFY_EMAIL_INPUT:
+            return {
+                ...state,
+                welcomeInputs: {
+                    ...state.welcomeInputs,
+                    emailInput: action.text
+                }
+            };
+        case welcomeInputActions.MODIFY_PASSWORD_INPUT:
+            return {
+                ...state,
+                welcomeInputs: {
+                    ...state.welcomeInputs,
+                    passwordInput: action.text
+                }
+            };
+        case welcomeInputActions.RESET_INPUTS:
+            return {
+                ...state,
+                welcomeInputs: {
+                    usernameInput: '',
+                    emailInput: '',
+                    passwordInput: ''
+                }
+            };
+        default:
+            return state;
+    };
+};
