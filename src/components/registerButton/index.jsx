9cac1bbabd4f6369cf14730a3af76b8314251480
Jaime Millán <jmillangalvez@gmail.com>
2023-03-19 00:12:38 +0100

    CREATE registerButton

---
+++ src/components/registerButton/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { styles } from './styles';
+import { registerStart } from '../../redux/actions/registerActions';
+
+const LoginButton = ({ navigation }) => {
+    const dispatch = useDispatch();
+
+    return (
+        <View style={styles.buttonView}>
+            <TouchableOpacity
+                onPress={() => dispatch(registerStart())}
+                style={styles.button}>
+                <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export default LoginButton;
