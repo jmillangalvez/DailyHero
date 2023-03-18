d15fdff11244ec6f7a83aa56fec6ab77b0853b84
Jaime Millán <jmillangalvez@gmail.com>
2023-03-18 16:43:41 +0100

    ADD redux actions

---
+++ src/redux/actions/welcomeInputActions.js
@@ -0,0 +1,25 @@
+export const welcomeInputActions = {
+    MODIFY_USERNAME_INPUT: 'MODIFY_USERNAME_INPUT',
+    MODIFY_EMAIL_INPUT: 'MODIFY_EMAIL_INPUT',
+    MODIFY_PASSWORD_INPUT: 'MODIFY_PASSWORD_INPUT',
+    RESET_INPUTS: 'RESET_INPUTS'
+}
+
+export const modifyUsernameInput = text => ({
+    type: 'MODIFY_USERNAME_INPUT',
+    text,
+});
+
+export const modifyEmailInput = text => ({
+    type: 'MODIFY_EMAIL_INPUT',
+    text,
+});
+
+export const modifyPasswordInput = text => ({
+    type: 'MODIFY_PASSWORD_INPUT',
+    text,
+});
+
+export const resetInputs = () => ({
+    type: 'RESET_INPUTS'
+});
