02aedce92dbf92f8d519634ced0d063836a6849d
Jaime Millán <jmillangalvez@gmail.com>
2023-03-18 16:42:17 +0100

    ADD reset inputs actions on welcome buttons

--- src/components/welcomeButton/index.jsx
+++ src/components/welcomeButton/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { useDispatch } from 'react-redux';
 import { styles } from './styles';
+import { resetInputs } from '../../redux/actions/welcomeInputActions';
 
 const WelcomeButton = ({ text, navigation }) => {
+    const dispatch = useDispatch();
+
     return (
         <View style={styles.buttonView}>
             <TouchableOpacity
-                onPress={() => navigation.navigate(text, {})}
+                onPress={() => {
+                    dispatch(resetInputs());
+                    navigation.navigate(text, {});
+                }}
                 style={styles.button}>
                 <Text style={styles.buttonText}>{text}</Text>
             </TouchableOpacity>
